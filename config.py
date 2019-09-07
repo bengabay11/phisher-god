f4fd9ab06c2876a8e9a7b504320e867fab2f83f8
ben <ben.gabay38@gmail.com>
2019-09-07 20:38:04 +0300

    updated config file

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ HOST = '0.0.0.0'
 PORT = 80
 
 GMAIL_ROUTE = "/static/gmail/login"
+FACEBOOK_ROUTE = "/static/facebook/login"
+INSTAGRAM_ROUTE = "/static/instagram/login"
+
 STATIC_FOLDER = "static"
 
 USERS_FILE_PATH = "users.json"
@@ -10,3 +13,5 @@ READ_FILE_MODE = "r"
 WRITE_FILE_MODE = "w"
 
 GMAIL_REDIRECT_URL = "http://www.gmail.com"
+FACEBOOK_REDIRECT_URL = "http://www.facebook.com"
+INSTAGRAM_REDIRECT_URL = "http://www.instagram.com"
