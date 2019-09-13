1bbb4cb0f38282bdbcc6fa62ba29885d6e951f9f
ben <ben.gabay38@gmail.com>
2019-09-13 22:24:38 +0300

    added strings to the configuration

--- static/instagram/src/config.js
+++ static/instagram/src/config.js
@@ -1,5 +1,15 @@
 export const config = {
-    stateKeyName: "state"
+    title: "Instagram",
+    usernameInputPlaceHolder: "Phone number, username or email",
+    passwordInputPlaceHolder: "Password",
+    signUpMessage: "Don't have an account?",
+    loginButtonText: "Log In",
+    loginWithFacebookButtonText: "Log In with Facebook",
+    forgotPasswordButtonText: "Forgot password?",
+    signUpButtonText: "Sign Up",
+    localStorageStateKey: "state",
+    downloadAppMessage: "Get the app.",
+    copyRightsMessage: "© 2019 INSTAGRAM FROM FACEBOOK",
 };
 
 export default config;
