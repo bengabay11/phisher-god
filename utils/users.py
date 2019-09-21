418dca4fb525a4c586c8155ae910a30f66f77443
ben <ben.gabay38@gmail.com>
2019-09-21 19:07:07 +0300

    handle bug when the users file doesnt exist, the code creates it

--- utils/users.py
+++ utils/users.py
@@ -1,18 +1,21 @@
 import json
+import os
 
 import config
 
 
 def add_user(username, password, account_type):
-    with open(config.USERS_FILE_PATH, config.READ_FILE_MODE) as users_file:
-        users = json.load(users_file)
+    if os.path.exists(config.USERS_FILE_PATH):
+        with open(config.USERS_FILE_PATH, config.READ_FILE_MODE) as users_file:
+            users = json.load(users_file)
+    else:
+        users = []
 
-        new_user = {
-            "username": username,
-            "password": password,
-            "account_type": account_type
-        }
-        users.append(new_user)
-
+    new_user = {
+        "username": username,
+        "password": password,
+        "account_type": account_type
+    }
+    users.append(new_user)
     with open(config.USERS_FILE_PATH, config.WRITE_FILE_MODE) as users_file:
         json.dump(users, users_file)
